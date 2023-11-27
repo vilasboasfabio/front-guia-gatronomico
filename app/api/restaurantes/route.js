58c9eacf5795b12156c35ba57ac9480fe40f277a
giobasiliox <125027610+giobasiliox@users.noreply.github.com>
2023-11-27 17:14:32 -0300

    feat: gloria deus

--- app/api/restaurantes/route.js
+++ app/api/restaurantes/route.js
@@ -1,19 +1,30 @@
 import axios from 'axios';
 import {NextResponse} from 'next/server';
 
-const url = "http://localhost:4005/" + "restaurante";
+const url = process.env.BASE_URL + "restaurante";
 
 export async function GET() {
 
     try{
+        console.log("Passou no try")
         const resposta = await axios.get(url);
         console.log("[Resposta]", resposta.data)
         return NextResponse.json(resposta.data);
 
     }catch(erro){
-        console.log("[Order_Get]", erro);
-        return new NextResponse("Erro do servidor teste", {status: 500});
+        console.log("[Order_Post]", erro);
+        // Verificar se o erro é um erro de resposta da API (como um erro 404)
+        if (erro.response) {
+            // Se o status do erro for 404, retorna o array de erros da API
+            if (erro.response.status === 400) {
+                return NextResponse.json(erro.response.data, { status: 400 });
+            }
+            // Se for outro tipo de erro de resposta, você pode adicionar tratamentos adicionais aqui
+        }
+        // Para erros que não são de resposta da API, retorna um erro 500
+        return new NextResponse("Erro do servidor dentro do route", {status: 500});
     }
+    
 
 }
 
@@ -30,6 +41,17 @@ export async function POST(request) {
         return NextResponse.json(resposta.data);
     } catch(erro){
         console.log("[Order_Post]", erro);
+        // Verificar se o erro é um erro de resposta da API (como um erro 404)
+        if (erro.response) {
+            // Se o status do erro for 404, retorna o array de erros da API
+            if (erro.response.status === 400) {
+                console.log(erro.response.data.erros)
+                return NextResponse.json(erro.response.data.erros, { status: 400 });
+            }
+            // Se for outro tipo de erro de resposta, você pode adicionar tratamentos adicionais aqui
+        }
+        // Para erros que não são de resposta da API, retorna um erro 500
         return new NextResponse("Erro do servidor dentro do route", {status: 500});
     }
+    
 }
