fd5fd2c34e50d180740fc57f77bd50cf5a1f6f6b
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-12-04 16:58:51 -0300

    feat: add another image

--- app/components/Filter.jsx
+++ app/components/Filter.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const FilterDropdown = ({ label, options, onChange }) => (
-  <div className="flex flex-col w-full max-w-xs mx-auto shadow-md rounded-md overflow-hidden">
-    <div className="px-4 py-2 bg-gray-700 text-white">
+  <div className="flex flex-col w-full max-w-xs mx-auto shadow-md rounded-xl overflow-hidden">
+    <div className="px-4 py-2 bg-bronze text-white">
       <h2 className="text-lg font-semibold">{label}</h2>
     </div>
     <div className="px-4 py-2">
