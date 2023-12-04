49dcfb4916837e70870e9257e6531508908d5009
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-12-04 19:44:52 -0300

    feat: add new filter color

--- app/components/Filter.jsx
+++ app/components/Filter.jsx
@@ -5,7 +5,7 @@ const FilterDropdown = ({ label, options, onChange }) => (
     <div className="px-4 py-2 bg-bronze text-white">
       <h2 className="text-lg font-semibold">{label}</h2>
     </div>
-    <div className="px-4 py-2">
+    <div className="px-4 py-2 bg-lbronze">
       <select onChange={onChange} className="w-full text-base outline-none transition duration-500 ease-in-out transform border-transparent rounded-lg bg-lbronze focus:border-gray-500 focus:bg-white focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
         {options.map(option => (
           <option key={option.value} value={option.value}>
